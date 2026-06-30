f790687c4f48256f0323f027855d02a0289d7702
Emmlass <enaibreemma@gmail.com>
2026-06-30 17:59:59 +0100

    Continuation of my folder structure

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 // adding the product routes to make it easy to just parse "/" at the product.js file
-const productRoutes = require('/api/routes/products');
+const productRoutes = require("./api/routes/products");
 
 app.use('/products', productRoutes);
-
+ 
 // app.use((req,res,next)=>{
 //     res.status(200).json({
 //         message: "it works"
