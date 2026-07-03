db27bede692f839b4a12b4861ceb46134239aa1c
Emmlass <enaibreemma@gmail.com>
2026-07-03 14:09:38 +0100

    Basic api created for the shop

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 // adding the product routes to make it easy to just parse "/" at the product.js file
 const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
 
 app.use('/products', productRoutes);
+app.use('/orders',orderRoutes);
  
 // app.use((req,res,next)=>{
 //     res.status(200).json({
