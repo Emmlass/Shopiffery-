3fa724b278f752a1cd01ca52f009b7e15f6d2e2c
Emmlass <enaibreemma@gmail.com>
2026-07-06 18:49:05 +0100

    Error handling setup

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 // adding the product routes to make it easy to just parse "/" at the product.js file
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+
+// using morgan in dev mode to log the requests made to the server
+app.use(morgan("dev"));
+
 
 // ROutes that handles requests
 app.use('/products', productRoutes);
@@ -15,4 +20,20 @@ app.use('/orders',orderRoutes);
 
 // })
 
+// to handle requests that are not handled by the above routes
+app.use((req,res,next)=>{
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);// gives the error name and passes it to the next middleware which is the error handler below
+ })
+
+//  handles the error passed from the above middleware and sends a response to the client
+ app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message
+        }
+    })
+ })
 module.exports= app;
