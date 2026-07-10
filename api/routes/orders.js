b489ea8fc80cc08d6a4d2a2b92973c94d20c3e80
Emmlass <enaibreemma@gmail.com>
2026-07-10 17:50:25 +0100

    cleaning the code, commenting and adding morgan

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+// Handles incoming GET requests to /orders
 router.get("/", (req,res,next)=>{
     res.status(200).json({
         message: "Orders were fetched"
     });
 }); 
+
+// Handles POST requests to /orders
 router.post("/", (req,res,next)=>{
     res.status(201).json({
-        message: "Order were created"
+        message: "Order was created"
     });
 });
 
+// Handles GET requests to /orders/:orderId
 router.get("/:orderId",(req,res,next)=>{
     res.status(200).json({
         message: "Order details",
@@ -19,6 +23,7 @@ router.get("/:orderId",(req,res,next)=>{
     })
 })
 
+// Handles DELETE requests to /orders/:orderId
 router.delete("/:orderId",(req,res,next)=>{
     res.status(200).json({
         message: "Deleted order",
